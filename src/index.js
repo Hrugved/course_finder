69a8ec28e944444fc3b0fbf71b36e0b27aa71f6b
hrugved <whrugved@gmail.com>
2022-04-02 04:27:22 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = process.env.PORT
+const port = process.env.PORT || PORT
 
 app.listen(port,() => {
     console.log('Server is up and running on port ' + port)
