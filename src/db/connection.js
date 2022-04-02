30b5b2fb36741bb139f8d537e5968d81f796745d
hrugved <whrugved@gmail.com>
2022-04-02 22:24:51 +0530

    change db connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host     : process.env.MYSQL_HOST,
-  user     : process.env.MYSQL_USER,
-  password : process.env.MYSQL_PASSWORD,
-  database : process.env.MYSQL_database
+host     : 'ec2-54-165-91-173.compute-1.amazonaws.com',
+user     : 'root',
+password : '314159',
+database : 'course_finder'
 });
 module.exports = connection
