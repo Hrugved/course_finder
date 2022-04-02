78e4eb417eedc6ec2723ed6c3cdbc04c23ab44fa
hrugved <whrugved@gmail.com>
2022-04-02 22:27:10 +0530

    db

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-host     : 'ec2-54-165-91-173.compute-1.amazonaws.com',
+host     : '127.0.0.1',
 user     : 'root',
 password : '314159',
 database : 'course_finder'
